e67b95299c25c0758024d372f48597e86f286272
gaozhihua520 <2423887429@qq.com>
2018-09-26 23:13:46 +0800

    登录-ajax请求登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -21,7 +21,7 @@ $(function(){
           validators: {
             // 2.2 非空校验
             notEmpty: {
-              message: "用户名不存在"
+              message: "用户名不能为空"
             },
             // 2.3 长度要求，2~6
             stringLength: {
@@ -30,7 +30,7 @@ $(function(){
               message: "用户名长度必须是 2-6 位"
             },
             callback: {
-              message: "用户名不能为空"
+              message: "用户名不存在"
             }
           }
         },
@@ -51,8 +51,57 @@ $(function(){
             callback: {
               message: "密码错误"
             }
-        }
+          }
         }
       }
     });
+
+    // 2  当表单校验成功时，会触发success.form.bv事件，此时会提交表单，这时候，通常我们需要禁止表单的自动提交，使用ajax进行表单的提交。
+  //  (1) 校验通过的时候，默认将表单继续提交，会跳转页面，需要在校验通过后阻止默认的提交，
+  // (2) 校验失败，表单本身就会阻止默认提交
+
+    $("#form").on('success.form.bv', function (e) {
+      e.preventDefault();
+      //使用ajax提交逻辑
+
+      $.ajax({
+        type: "post",
+        url: "/employee/employeeLogin",
+        // 通过表单序列化的方法快速获取到表单的值
+        data: $('#form').serialize(),
+        dataType: "json",
+        success: function(info){
+          console.log(info);
+          // 如果登录成功，就跳转页面
+          if(info.success){
+            location.href = "index.html";
+          }
+
+          // // 如果登录失败
+          if(info.error === 1000){
+            // 说明用户名不存在
+            // updateStatus(用户名，校验失败，错误信息)
+            $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+            // alert("用户名不存在");
+          }
+          if(info.error === 1001){
+            // 说明密码错误，将表单密码校验状态从成功更新为失败
+            // alert("密码错误");
+            $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+          }
+        }
+      })
+  });
+
+
+
+  // 3 表单重置
+  // 属性值为reset的元素，注册点击事件
+  $('[type="reset"]').click(function() {
+    console.log( 1111 );
+    // 除了重置文本, 还要重置校验状态，利用插件的方法
+    // resetForm(),传true，表单内容和校验状态都要重置，false，只重置校验状态
+    $('#form').data("bootstrapValidator").resetForm();
+  });
+
 })
